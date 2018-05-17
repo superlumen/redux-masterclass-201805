8f4c048eeb83c26ab45692a6ff93e132b1713eb2
Callum Macdonald <git@callum-macdonald.com>
2018-05-17 19:55:54 +0200

    Add the Provider.

--- app/src/App.js
+++ app/src/App.js
@@ -12,19 +12,22 @@ const empty = {
 };
 
 const reducer = (state = empty, action) => state;
+const store = createStore(reducer);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Provider>
     );
   }
 }
